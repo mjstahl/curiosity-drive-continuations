04a9286da6e1e6bb850633415a4d18687da88cd5
Mark Stahl <mjstahl@gmail.com>
2015-05-30 20:36:05 -0400

    more operators

--- globals.js
+++ globals.js
@@ -1,8 +1,20 @@
 module.exports = {
-	"+" : function(a, b) {
-		return a + b; 
-	},
-	"*" : function(a, b) {
-		return a * b 
-	}
+	"*": function(a, b) {
+        return a * b;
+    },
+    ";": function(a, b) {
+        return b;
+    },
+    "+": function(a, b) {
+        return a + b;
+    },
+    "-": function(a, b) {
+        return a - b;
+    },
+    "%": function(a, b) {
+        return a % b;
+    },
+    ">": function(a, b) {
+        return this[a > b];
+    }
 };
